b5e8f8f0706f989f40e63f5c770b59d7aa277e53
KnaiQ7 <131540921+KnaiQ7@users.noreply.github.com>
2025-11-10 19:50:44 -0300

    eliminación componente 1 y 2
    daba error el bootstrap tambien xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Componente1Component } from './components/componente1/componente1.component';
-import { Componente2Component } from './components/componente2/componente2.component';
 import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
@@ -13,8 +11,6 @@ import { FooterComponent } from './components/layout/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component,
-    Componente2Component,
     PaginaPrincipalComponent,
     HeaderComponent,
     FooterComponent
