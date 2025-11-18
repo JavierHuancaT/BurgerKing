794610ed507ae61a8c172c373bd1c943bfbca47b
KnaiQ7 <131540921+KnaiQ7@users.noreply.github.com>
2025-11-18 09:18:07 -0300

    detalles menores 2 login xd

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -10,17 +10,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; 
 
 /**
- * "Buenas prácticas de desarrollo" y NFR 4.1 (Rendimiento)
+ * (Rendimiento)
  * Se importa el archivo de rutas propio de este módulo.
  * Esto permite el "Lazy Loading" (carga diferida), una práctica
  * de arquitectura que mejora el rendimiento de la carga inicial.
  */
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component'; // <-- 2. IMPORTA TU COMPONENTE
+import { LoginComponent } from './login/login.component'; // <-- 2. IMPORTA El COMPONENTE
 
 /**
  * Módulo de Autenticación (AuthModule). (HDU1, HDU2)
- * RÚBRICA: "Buenas prácticas de desarrollo"
  * Este archivo define un "Feature Module". Agrupa toda la lógica
  * de autenticación (login, registro) en un solo lugar,
  * manteniéndolo separado y encapsulado del resto de la app.
@@ -28,26 +27,23 @@ import { LoginComponent } from './login/login.component'; // <-- 2. IMPORTA TU C
 @NgModule({
  /**
   * 'declarations': Componentes que pertenecen a este módulo.
-  * RÚBRICA: "Buenas prácticas de desarrollo"
-  * Aquí se declara 'LoginComponent'. Esto soluciona el error NG6007,
-  * ya que un componente SÓLO debe ser declarado en UN módulo.
+  * Aquí se declara 'LoginComponent', un componente SÓLO debe ser declarado en UN módulo.
   * (No debe estar en app.module.ts).
   */
  declarations: [
-   LoginComponent // <-- 3. DECLARA TU COMPONENTE AQUÍ
+   LoginComponent 
  ],
  /**
   * 'imports': Módulos externos que este módulo necesita.
   */
  imports: [
    /**
-    * RÚBRICA: "Uso de directivas: NgIf, Ngif-else, NgFor"
     * Importa 'CommonModule', que es lo que da acceso a las
     * directivas estructurales como *ngIf (usada para los errores).
     */
    CommonModule,
    AuthRoutingModule,  // Para las rutas de este módulo
-   FormsModule         // (IMPORTANTE) Para que [(ngModel)] funcione
+   FormsModule         // Para que [(ngModel)] funcione
  ]
 })
 export class AuthModule { }
